63ea3be8856b0fb8b9d9566c6f2f6cf11a5292b9
pedjo95 <tec.ped95@gmail.com>
2020-05-19 22:54:41 -0300

    Connecting to db

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
 
-const indexRoute = require('./routes/index-route');
-const productRoute = require('./routes/product-route');
+// connecting to db
+mongoose.connect(
+  "mongodb+srv://pedro@cluster0-oa8p7.azure.mongodb.net/admin?replicaSet=Cluster0-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Cluster0-shard-0&3t.databases=admin,test"
+);
+
+// loading routes
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-route");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', indexRoute);
-app.use('/products', productRoute);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
