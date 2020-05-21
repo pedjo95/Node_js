03ec1be5a29159f18cb6a41f84622adbdd7b6113
pedjo95 <tec.ped95@gmail.com>
2020-05-20 22:47:38 -0300

    Deleting a product

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -88,5 +88,17 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-  res.status(200).send(req.body);
+  Product
+    .findOneAndRemove(req.body.id)
+    .then(x => {
+      res.status(200).send({
+        message: 'Produto removido com sucesso'
+      });
+    })
+    .catch(error => {
+      res.status(400).send({
+        message: 'Falha ao remover o produto',
+        data: error
+      });
+    });
 };
--- node-str/src/routes/product-route.js
+++ node-str/src/routes/product-route.js
@@ -7,7 +7,7 @@ router.get('/:slug', controller.getBySlug);
 router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
-router.put('/:id', controller.put)
+router.put('/:id', controller.put);
 router.delete('/', controller.delete)
 
 module.exports = router;
